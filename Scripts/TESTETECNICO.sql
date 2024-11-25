9640f29e1963ed61b649b76969fc2900db4cea06
Henrique Polzella <67474625+HenriquePolzella@users.noreply.github.com>
2024-11-25 14:01:55 -0300

    Add files via upload

---
+++ Scripts/TESTETECNICO.sql
@@ -0,0 +1,68 @@
+CREATE OR ALTER PROCEDURE dbo.CriarBancoDeDadosETabelas
+AS
+BEGIN
+    -- Criar o banco de dados
+    IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'TesteTecnico')
+    BEGIN
+        CREATE DATABASE TesteTecnico;
+    END
+
+    
+    -- Criar a tabela Profissionais
+    IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Profissionais')
+    BEGIN
+        CREATE TABLE Profissionais (
+            Id_Profissional INT PRIMARY KEY IDENTITY(1,1),
+            Nome_Profissional VARCHAR(100) NOT NULL,
+            Nome_Especialidade CHAR(50) NOT NULL,
+            Tipo_Documento CHAR(9) NOT NULL,
+            Numero_Documento CHAR(8) NOT NULL
+        );
+    END
+
+    -- Criar a tabela Especialidades
+    IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Especialidades')
+    BEGIN
+        CREATE TABLE Especialidades (
+            Id_Especialidade INT PRIMARY KEY IDENTITY(1,1),
+            Nome_Especialidade CHAR(50) NOT NULL,
+            Tipo_Documento CHAR(10) NOT NULL
+        );
+    END
+
+    -- Inserir os dados na tabela Especialidades
+    IF NOT EXISTS (SELECT * FROM Especialidades)
+    BEGIN
+        INSERT INTO Especialidades (Nome_Especialidade, Tipo_Documento)
+        VALUES
+            ('Cardiologia', 'CRM'),
+            ('Ortopedia', 'CRM'),
+            ('Pediatria', 'CRM'),
+            ('Dermatologia', 'CRM'),
+            ('Ginecologia', 'CRM'),
+            ('Neurologia', 'CRM'),
+            ('Psiquiatria', 'CRM'),
+            ('Anestesiologia', 'CRM'),
+            ('Radiologia', 'CRM'),
+            ('Endocrinologia', 'CRM'),
+            ('Nutricionista Clínico', 'CRN'),
+            ('Nutrição Esportiva', 'CRN'),
+            ('Nutrição Oncológica', 'CRN'),
+            ('Fisioterapia Ortopédica', 'CREFITO'),
+            ('Fisioterapia Neurológica', 'CREFITO'),
+            ('Fisioterapia Respiratória', 'CREFITO'),
+            ('Ortodontia', 'CRO'),
+            ('Implantodontia', 'CRO'),
+            ('Endodontia', 'CRO'),
+            ('Enfermagem Geral', 'COREN'),
+            ('Enfermagem Obstétrica', 'COREN'),
+            ('Psicologia Clínica', 'CRP'),
+            ('Psicologia Organizacional', 'CRP'),
+            ('Neuropsicologia', 'CRP'),
+            ('Farmácia Clínica', 'CRF'),
+            ('Farmácia Hospitalar', 'CRF'),
+            ('Análises Clínicas', 'CRF'),
+            ('Personal Trainer', 'CREF'),
+            ('Preparação Física', 'CREF');
+    END
+END;
